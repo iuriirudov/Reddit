73b7aef7b5f74468afdb46e5d6aea31f793cac0d
Iurii Rudov <iuriirudov@gmail.com>
2022-03-22 10:12:11 -0400

    Project setup

---
+++ src/main/java/com/reddit/reddit/model/NotificationEmail.java
@@ -0,0 +1,4 @@
+package com.reddit.reddit.model;
+
+public class NotificationEmail {
+}
---
+++ src/main/java/com/reddit/reddit/model/Subreddit.java
@@ -0,0 +1,42 @@
+package com.reddit.reddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import java.time.Instant;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Subreddit {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Subreddit name is required")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @OneToMany(fetch = LAZY)
+    private List<Post> posts;
+
+    private Instant created;
+
+    @ManyToOne(fetch = LAZY)
+    private User user;
+
+}
